800a8527b70cf71f6af3159396f9c277c59ccd8c
Carlos Santos <casantosmu@gmail.com>
2025-03-26 11:53:33 +0100

    Clear frontend code

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080", // Backend server
+                // changeOrigin: true, // Ensure the request appears to come from the frontend server
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 });
