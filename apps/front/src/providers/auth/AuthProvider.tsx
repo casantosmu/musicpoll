8cd610c41611e6f0dbd0846c4828e28b22a2c972
Carlos Santos <casantosmu@gmail.com>
2025-05-11 17:23:28 +0200

    Add loading on AuthProvider

--- apps/front/src/providers/auth/AuthProvider.tsx
+++ apps/front/src/providers/auth/AuthProvider.tsx
@@ -28,7 +28,18 @@ export default function AuthProvider({ children }: PropsWithChildren) {
             });
     }, []);
 
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-zinc-900">
+                <div className="flex items-center justify-center flex-col">
+                    <div className="animate-spin rounded-full h-16 w-16 border-3 border-t-green-500 border-b-green-500 border-r-transparent border-l-transparent"></div>
+                    <p className="mt-4 text-zinc-300 font-medium">Loading...</p>
+                </div>
+            </div>
+        );
+    }
+
     // eslint-disable-next-line react-x/no-unstable-context-value
     const value = { user, login, logout, isLoggedIn: !!user, isLoading };
-    return <AuthContext value={value}>{isLoading ? null : children}</AuthContext>;
+    return <AuthContext value={value}>{children}</AuthContext>;
 }
